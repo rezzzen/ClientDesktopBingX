a6111354ee1b3d5be858c0ca38c071d2c0a75427
Анатолий <myferm01@yandex.ru>
2025-06-03 11:17:42 +0300

    Добавьте файлы проекта.

---
+++ ClientDesktopBingX/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace ClientDesktopBingX
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            plotView1 = new OxyPlot.WindowsForms.PlotView();
+            CB_INSTRUMENTS = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            CB_TIMEFRAME = new ComboBox();
+            label3 = new Label();
+            label4 = new Label();
+            CB_INDICATORS = new ComboBox();
+            label5 = new Label();
+            L_TIME = new Label();
+            label6 = new Label();
+            L_RECOMMENDATION = new Label();
+            UD_COUNT_BARS = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)UD_COUNT_BARS).BeginInit();
+            SuspendLayout();
+            // 
+            // plotView1
+            // 
+            plotView1.Location = new Point(254, 35);
+            plotView1.Name = "plotView1";
+            plotView1.PanCursor = Cursors.Hand;
+            plotView1.Size = new Size(946, 532);
+            plotView1.TabIndex = 0;
+            plotView1.Text = "plotView1";
+            plotView1.ZoomHorizontalCursor = Cursors.SizeWE;
+            plotView1.ZoomRectangleCursor = Cursors.SizeNWSE;
+            plotView1.ZoomVerticalCursor = Cursors.SizeNS;
+            // 
+            // CB_INSTRUMENTS
+            // 
+            CB_INSTRUMENTS.DropDownStyle = ComboBoxStyle.DropDownList;
+            CB_INSTRUMENTS.FormattingEnabled = true;
+            CB_INSTRUMENTS.Location = new Point(26, 54);
+            CB_INSTRUMENTS.Name = "CB_INSTRUMENTS";
+            CB_INSTRUMENTS.Size = new Size(151, 28);
+            CB_INSTRUMENTS.TabIndex = 1;
+            CB_INSTRUMENTS.SelectedIndexChanged += CB_INSTRUMENTS_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(149, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Выбор инструмента";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(28, 85);
+            label2.Name = "label2";
+            label2.Size = new Size(149, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Выбор таймфрейма";
+            // 
+            // CB_TIMEFRAME
+            // 
+            CB_TIMEFRAME.DropDownStyle = ComboBoxStyle.DropDownList;
+            CB_TIMEFRAME.FormattingEnabled = true;
+            CB_TIMEFRAME.Location = new Point(26, 108);
+            CB_TIMEFRAME.Name = "CB_TIMEFRAME";
+            CB_TIMEFRAME.Size = new Size(151, 28);
+            CB_TIMEFRAME.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(8, 139);
+            label3.Name = "label3";
+            label3.Size = new Size(185, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Выбор количества баров";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(35, 192);
+            label4.Name = "label4";
+            label4.Size = new Size(142, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Выбор индикатора";
+            // 
+            // CB_INDICATORS
+            // 
+            CB_INDICATORS.DropDownStyle = ComboBoxStyle.DropDownList;
+            CB_INDICATORS.FormattingEnabled = true;
+            CB_INDICATORS.Location = new Point(24, 215);
+            CB_INDICATORS.Name = "CB_INDICATORS";
+            CB_INDICATORS.Size = new Size(151, 28);
+            CB_INDICATORS.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Italic, GraphicsUnit.Point, 204);
+            label5.Location = new Point(943, 9);
+            label5.Name = "label5";
+            label5.Size = new Size(154, 20);
+            label5.TabIndex = 9;
+            label5.Text = "Версия от 12.05.2025";
+            // 
+            // L_TIME
+            // 
+            L_TIME.AutoSize = true;
+            L_TIME.Location = new Point(1015, 570);
+            L_TIME.Name = "L_TIME";
+            L_TIME.Size = new Size(185, 20);
+            L_TIME.TabIndex = 10;
+            L_TIME.Text = "Выбор количества баров";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(24, 494);
+            label6.Name = "label6";
+            label6.Size = new Size(164, 20);
+            label6.TabIndex = 11;
+            label6.Text = "Общая рекомендация";
+            // 
+            // L_RECOMMENDATION
+            // 
+            L_RECOMMENDATION.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            L_RECOMMENDATION.Location = new Point(24, 528);
+            L_RECOMMENDATION.Name = "L_RECOMMENDATION";
+            L_RECOMMENDATION.Size = new Size(164, 39);
+            L_RECOMMENDATION.TabIndex = 12;
+            L_RECOMMENDATION.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // UD_COUNT_BARS
+            // 
+            UD_COUNT_BARS.Location = new Point(28, 162);
+            UD_COUNT_BARS.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            UD_COUNT_BARS.Name = "UD_COUNT_BARS";
+            UD_COUNT_BARS.Size = new Size(150, 27);
+            UD_COUNT_BARS.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1236, 609);
+            Controls.Add(UD_COUNT_BARS);
+            Controls.Add(L_RECOMMENDATION);
+            Controls.Add(label6);
+            Controls.Add(L_TIME);
+            Controls.Add(label5);
+            Controls.Add(CB_INDICATORS);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(CB_TIMEFRAME);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(CB_INSTRUMENTS);
+            Controls.Add(plotView1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "CDA - Commodity Data Analysis";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)UD_COUNT_BARS).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private ComboBox CB_INSTRUMENTS;
+        private Label label1;
+        private Label label2;
+        private ComboBox CB_TIMEFRAME;
+        private Label label3;
+        private Label label4;
+        private ComboBox CB_INDICATORS;
+        private Label label5;
+        private Label L_TIME;
+        private Label label6;
+        private Label L_RECOMMENDATION;
+        private NumericUpDown UD_COUNT_BARS;
+    }
+}
---
+++ ClientDesktopBingX/Form1.cs
@@ -0,0 +1,454 @@
+﻿using MongoDB.Driver;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Windows.Forms;
+using System.Globalization;
+using MongoDB.Bson.Serialization.Conventions;
+using System;
+using System.Timers;
+
+namespace ClientDesktopBingX
+{
+    public partial class Form1 : Form
+    {
+        private readonly IMongoClient _mongoClient;
+        private IMongoCollection<CandleData> _currentCollection;
+        private readonly System.Timers.Timer _timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+            plotView1.Controller = new PlotController();
+            plotView1.Controller.UnbindAll();
+
+            // Панорамирование: зажать ЛКМ + перемещение мыши
+            plotView1.Controller.BindMouseDown(OxyMouseButton.Left, PlotCommands.PanAt);
+
+            // Масштабирование колесом мыши
+            plotView1.Controller.BindMouseWheel(PlotCommands.ZoomWheel);
+
+
+
+
+            _mongoClient = new MongoClient("mongodb://localhost:27017");
+
+            var conventionPack = new ConventionPack { new IgnoreExtraElementsConvention(true) };
+            ConventionRegistry.Register("IgnoreExtraElements", conventionPack, t => true);
+
+            // Инициализация таймера для обновления времени
+            _timer = new System.Timers.Timer(1000);
+            _timer.Elapsed += UpdateTime;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+
+            // Инициализация элементов управления
+            CB_TIMEFRAME.Items.AddRange(new[] { "5M", "10M", "30M", "1H", "4H", "1D", "1W" });
+            CB_TIMEFRAME.SelectedIndex = 0;
+
+            CB_INDICATORS.Items.AddRange(new[] { "None", "SMA 20", "EMA 20", "RSI 14" });
+            CB_INDICATORS.SelectedIndex = 0;
+
+            UD_COUNT_BARS.Minimum = 1;
+            UD_COUNT_BARS.Value = 100;
+
+            // Подписка на события
+            CB_TIMEFRAME.SelectedIndexChanged += ReloadDataEvent;
+            CB_INDICATORS.SelectedIndexChanged += ReloadDataEvent;
+            UD_COUNT_BARS.ValueChanged += ReloadDataEvent;
+            this.Load += Form1_Load;
+            CB_INSTRUMENTS.SelectedIndexChanged += CB_INSTRUMENTS_SelectedIndexChanged;
+        }
+
+
+        private void ReloadDataEvent(object sender, EventArgs e)
+        {
+            if (CB_INSTRUMENTS.SelectedItem != null)
+                LoadDataAndPlot();
+        }
+
+        private void UpdateTime(object sender, ElapsedEventArgs e)
+        {
+            SafeUpdateLabel(L_TIME, DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+        }
+
+        private void SafeUpdateLabel(Label label, string text)
+        {
+            if (label.InvokeRequired)
+            {
+                label.Invoke(new Action(() => label.Text = text));
+            }
+            else
+            {
+                label.Text = text;
+            }
+        }
+
+
+
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await LoadInstrumentsAsync();
+            await LoadDataAndPlot(); // Загрузка данных после инициализации инструментов
+        }
+
+        private async Task LoadInstrumentsAsync()
+        {
+            try
+            {
+                CB_INSTRUMENTS.Enabled = false;
+                Cursor = Cursors.WaitCursor;
+
+                var database = _mongoClient.GetDatabase("CDA");
+
+                // Получаем все коллекции
+                var collections = await database.ListCollectionNamesAsync();
+                var filteredCollections = (await collections.ToListAsync())
+                    .Where(c => !c.StartsWith("system."))
+                    .ToList();
+
+                // Создаем список для хранения информации о коллекциях
+                var collectionStats = new List<Tuple<string, long>>();
+
+                // Параллельно получаем количество документов
+                var tasks = filteredCollections.Select(async collectionName =>
+                {
+                    var collection = database.GetCollection<BsonDocument>(collectionName);
+                    var count = await collection.CountDocumentsAsync(FilterDefinition<BsonDocument>.Empty);
+                    return (Name: collectionName, Count: count);
+                });
+
+                var results = await Task.WhenAll(tasks);
+
+                // Сортируем по убыванию количества данных
+                var sortedCollections = results
+                    .OrderByDescending(x => x.Count)
+                    .ToList();
+
+                // Формируем список инструментов
+                var instruments = sortedCollections
+                    .Select(c => c.Name.Replace("_5M", "").Replace("_", "/"))
+                    .ToList();
+
+                SafeUpdateComboBox(instruments);
+                _currentCollection = null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки инструментов: {ex.Message}");
+            }
+            finally
+            {
+                CB_INSTRUMENTS.Enabled = true;
+                Cursor = Cursors.Default;
+            }
+        }
+
+        private void SafeUpdateComboBox(List<string> instruments)
+        {
+            if (CB_INSTRUMENTS.InvokeRequired)
+            {
+                CB_INSTRUMENTS.Invoke(new Action(() =>
+                {
+                    CB_INSTRUMENTS.DataSource = instruments;
+                    if (instruments.Any()) CB_INSTRUMENTS.SelectedIndex = 0;
+                }));
+            }
+            else
+            {
+                CB_INSTRUMENTS.DataSource = instruments;
+                if (instruments.Any()) CB_INSTRUMENTS.SelectedIndex = 0;
+            }
+        }
+
+        private async Task LoadDataAndPlot()
+        {
+            if (_currentCollection == null) return;
+
+            try
+            {
+                plotView1.Model = null;
+                Cursor = Cursors.WaitCursor;
+
+                var count = (int)UD_COUNT_BARS.Value;
+                var timeframe = CB_TIMEFRAME.SelectedItem.ToString();
+
+                // Загрузка всех 5M баров
+                var sortDefinition = Builders<CandleData>.Sort
+                    .Descending(d => d.Date)
+                    .Descending(d => d.Time);
+
+                var fiveMinuteCandles = await _currentCollection.Find(_ => true)
+                    .Sort(sortDefinition)
+                    .Limit(1000) // Берем больше данных для агрегации
+                    .ToListAsync();
+
+                fiveMinuteCandles = fiveMinuteCandles.OrderBy(c => c.Timestamp).ToList();
+
+                // Агрегация в выбранный таймфрейм
+                var aggregatedCandles = AggregateCandles(fiveMinuteCandles, timeframe)
+                    .TakeLast(count) // Берем последние N баров
+                    .ToList();
+
+                // Создаем новую модель графика
+                var plotModel = new PlotModel
+                {
+                    Title = $"{CB_INSTRUMENTS.SelectedItem} [{CB_TIMEFRAME.SelectedItem}]",
+                    TextColor = OxyColors.Gray,
+                    PlotAreaBorderColor = OxyColors.Gray,
+                    IsLegendVisible = true, // Показать легенду
+                    
+                };
+
+                // Добавляем оси
+                var dateAxis = new DateTimeAxis
+                {
+                    Position = AxisPosition.Bottom,
+                    StringFormat = "HH:mm",
+                    Title = "Time",
+                    MajorGridlineStyle = LineStyle.Solid,
+                    MinorGridlineStyle = LineStyle.Dot
+                };
+
+                var valueAxis = new LinearAxis
+                {
+                    Position = AxisPosition.Left,
+                    Title = "Price",
+                    MajorGridlineStyle = LineStyle.Solid,
+                    MinorGridlineStyle = LineStyle.Dot
+                };
+
+                plotModel.Axes.Add(dateAxis);
+                plotModel.Axes.Add(valueAxis);
+
+                // Добавляем свечной график
+                var candleSeries = new CandleStickSeries
+                {
+                    Title = "Candles",
+                    Color = OxyColors.DarkGray,
+                    IncreasingColor = OxyColors.Green,
+                    DecreasingColor = OxyColors.Red
+                };
+
+                foreach (var candle in aggregatedCandles)
+                {
+                    candleSeries.Items.Add(new HighLowItem(
+                        DateTimeAxis.ToDouble(candle.Timestamp),
+                        candle.High,
+                        candle.Low,
+                        candle.Open,
+                        candle.Close));
+                }
+
+                plotModel.Series.Add(candleSeries);
+
+                // Добавляем индикаторы
+                var selectedIndicator = CB_INDICATORS.SelectedItem?.ToString();
+                if (selectedIndicator != null && selectedIndicator != "None")
+                {
+                    AddIndicator(plotModel, aggregatedCandles, selectedIndicator);
+                }
+
+                // Обновляем рекомендации
+                UpdateRecommendation(aggregatedCandles);
+
+                // Привязываем модель к элементу управления
+                plotView1.Model = plotModel;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки данных: {ex.Message}");
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+            }
+        }
+
+
+
+        private List<CandleData> AggregateCandles(List<CandleData> sourceCandles, string timeframe)
+        {
+            var aggregated = new List<CandleData>();
+            TimeSpan interval;
+
+            switch (timeframe)
+            {
+                case "5M": interval = TimeSpan.FromMinutes(5); break;
+                case "10M": interval = TimeSpan.FromMinutes(10); break;
+                case "30M": interval = TimeSpan.FromMinutes(30); break;
+                case "1H": interval = TimeSpan.FromHours(1); break;
+                case "4H": interval = TimeSpan.FromHours(4); break;
+                case "1D": interval = TimeSpan.FromDays(1); break;
+                case "1W": interval = TimeSpan.FromDays(7); break;
+                default: throw new ArgumentException("Invalid timeframe");
+            }
+
+            DateTime currentGroupStart = DateTime.MinValue;
+            CandleData currentCandle = null;
+
+            foreach (var candle in sourceCandles.OrderBy(c => c.Timestamp))
+            {
+                var candleTime = candle.Timestamp;
+                var groupStart = GetGroupStartTime(candleTime, interval);
+
+                if (groupStart != currentGroupStart)
+                {
+                    if (currentCandle != null) aggregated.Add(currentCandle);
+                    currentGroupStart = groupStart;
+                    currentCandle = new CandleData
+                    {
+                        Date = groupStart.ToString("yyyy-MM-dd"),
+                        Time = groupStart.ToString("HH:mm"),
+                        Open = candle.Open,
+                        High = candle.High,
+                        Low = candle.Low,
+                        Close = candle.Close
+                    };
+                }
+                else
+                {
+                    currentCandle.High = Math.Max(currentCandle.High, candle.High);
+                    currentCandle.Low = Math.Min(currentCandle.Low, candle.Low);
+                    currentCandle.Close = candle.Close;
+                }
+            }
+
+            if (currentCandle != null) aggregated.Add(currentCandle);
+            return aggregated;
+        }
+
+        private DateTime GetGroupStartTime(DateTime timestamp, TimeSpan interval)
+        {
+            if (interval.TotalMinutes >= 1440) // Daily or weekly
+                return timestamp.Date;
+
+            var totalMinutes = (int)(timestamp.TimeOfDay.TotalMinutes / interval.TotalMinutes) * interval.TotalMinutes;
+            return timestamp.Date.AddMinutes(totalMinutes);
+        }
+
+        private void AddIndicator(PlotModel model, List<CandleData> candles, string indicator)
+        {
+            switch (indicator.Split(' ')[0])
+            {
+                case "SMA":
+                    var period = int.Parse(indicator.Split(' ')[1]);
+                    var smaValues = CalculateSMA(candles, period);
+                    var smaSeries = new LineSeries
+                    {
+                        Title = $"SMA {period}",
+                        Color = OxyColors.Blue
+                    };
+                    for (int i = 0; i < smaValues.Count; i++)
+                        smaSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(candles[i + period - 1].Timestamp), smaValues[i]));
+                    model.Series.Add(smaSeries);
+                    break;
+
+                    // Аналогично для других индикаторов
+            }
+        }
+
+        private List<double> CalculateSMA(List<CandleData> candles, int period)
+        {
+            var smaValues = new List<double>();
+            for (int i = period - 1; i < candles.Count; i++)
+            {
+                var sum = candles.Skip(i - period + 1).Take(period).Average(c => c.Close);
+                smaValues.Add(sum);
+            }
+            return smaValues;
+        }
+
+        private void UpdateRecommendation(List<CandleData> candles)
+        {
+            if (candles.Count < 2) return;
+
+            var lastCandle = candles.Last();
+            var prevCandle = candles[^2];
+
+            string recommendation;
+            if (lastCandle.Close > lastCandle.Open && lastCandle.Close > prevCandle.Close)
+            {
+                recommendation = "ПОКУПАТЬ";
+                L_RECOMMENDATION.ForeColor = Color.Green;
+            }
+            else
+            {
+                recommendation = "ПРОДАВАТЬ";
+                L_RECOMMENDATION.ForeColor = Color.Red;
+            }
+
+            SafeUpdateLabel(L_RECOMMENDATION, recommendation);
+        }
+
+        private async void CB_INSTRUMENTS_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (CB_INSTRUMENTS.SelectedItem == null) return;
+
+            try
+            {
+                var selectedInstrument = CB_INSTRUMENTS.SelectedItem.ToString();
+                var selectedTimeFrame = CB_TIMEFRAME.SelectedItem.ToString();
+                var collectionName = $"{selectedInstrument.Replace("/", "_")}_{selectedTimeFrame}";
+
+                var database = _mongoClient.GetDatabase("CDA");
+                _currentCollection = database.GetCollection<CandleData>(collectionName);
+
+                await LoadDataAndPlot();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
+
+    [BsonIgnoreExtraElements]
+    public class CandleData
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public double Open { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double Close { get; set; }
+
+        [BsonIgnore]
+        public DateTime Timestamp
+        {
+            get
+            {
+                if (DateTime.TryParseExact(
+                    $"{Date} {Time}",
+                    "yyyy-MM-dd HH:mm",
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.None,
+                    out var result))
+                {
+                    return result;
+                }
+                return DateTime.MinValue;
+            }
+        }
+    public CandleData Clone()
+    {
+        return new CandleData
+        {
+            Date = this.Date,
+            Time = this.Time,
+            Open = this.Open,
+            High = this.High,
+            Low = this.Low,
+            Close = this.Close
+        };
+    }
+}
